380f6afde5e6f9f45afbbfcd9fd65eae8d93dc3d
joswalt9 <156866636+joswalt9@users.noreply.github.com>
2024-09-16 19:38:12 -0400

    Add files via upload
    Add partials for nav and header
    Created functions for Get Courses, Save Courses, Get Individual Course
    Created course.ejs for viewing an individual course

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const fs = require("fs");
+const path = require("path");
+const coursesFilePath = path.join(__dirname, "courses.json");
 
 // Middleware
 app.use(express.json());
@@ -17,12 +20,53 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Course Placeholder
-let courses = [];
+// Get Courses Function
+function getCourses() {
+  if (fs.existsSync(coursesFilePath)) {
+    const data = fs.readFileSync(coursesFilePath, "utf8");
+    return JSON.parse(data);
+  }
+  return [];
+}
+
+// Save Courses Function
+function saveCourses(courses) {
+  fs.writeFileSync(coursesFilePath, JSON.stringify(courses, null, 2), "utf8");
+}
+
+// Get Individual Course
+app.get("/course/:index", (req, res) => {
+  const courses = getCourses();
+  const courseIndex = parseInt(req.params.index, 10);
+
+  console.log(`Requested course index: ${courseIndex}`); // Debug Line
+
+  if (courseIndex >= 0 && courseIndex < courses.length) {
+    const course = courses[courseIndex];
+    res.render("course", { course });
+  } else {
+    res.status(404).render("404");
+  }
+});
+
+// Add Course
+app.post("/addcourse", (req, res) => {
+  const courses = getCourses();
+  const newCourse = {
+    name: req.body.name,
+    description: req.body.description,
+    subject: req.body.subject,
+    credits: req.body.credits,
+  };
+  courses.push(newCourse);
+  saveCourses(courses);
+  res.redirect("/");
+});
 
 // Get Homepage
 app.get("/", (req, res) => {
-  res.render("index");
+  const courses = getCourses(); // Fetch the updated courses from the JSON file
+  res.render("index", { courses });
 });
 
 // Get Add Course page
